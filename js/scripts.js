dc6f704de96738ca364ac3e9d071a5fd0ef15a9d
skhan139 <146803728+skhan139@users.noreply.github.com>
2023-10-30 02:36:02 -0400

    This commit will finish updating the repository for 1.2 by adding the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-pokemonList = []
+let pokemonList = [
+  let pikachuArray =  [
+    name: 'pikachu'
+    height: '.4'
+    types: 'electric'
+
+  ];
+     
+  let squirtleArray = [
+    name: 'squirtle'
+    height: '.5'
+    types: 'water'
+
+  ];
+ 
+  let golemArray = [
+    name: 'golem'
+    height: '1'
+    types: 'Rock, Ground'
+  ]
+];
+
