3d6c7574d960be4ef9e553a4e16a3755a755e375
skhan139 <146803728+skhan139@users.noreply.github.com>
2023-11-02 18:16:43 -0400

    This commit will be a second update to the 1.3 assignment

--- js/scripts.js
+++ js/scripts.js
@@ -19,9 +19,9 @@ let pokemonList = [
 //Each object within the array is represented with i. The property picked to be used is formatted as pokemonList.property
 for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")");
-} 
 //the code within document.write is used when the height value of a pokemon exceeds >0.5
 if (pokemonList[i].height > 0.5) {
     document.write(' - Wow! a big one!');
-    document.write(' - Wow! a big one!'); // document.write
+}
+   document.write("<br>" + "<br>");
 }
